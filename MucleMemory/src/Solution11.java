bba6cb70d67d13798d5856f0756a031aa4e0ad6c
songyunfeng <56113272@qq.com>
2020-07-13 23:01:58 +0800

    July 13th.

--- MucleMemory/src/Solution11.java
+++ MucleMemory/src/Solution11.java
@@ -8,4 +8,19 @@
  * 滑动窗口法
  **/
 public class Solution11 {
+    public int maxArea(int[] height) {
+        int res = 0;
+        int left = 0;
+        int right = height.length - 1;
+
+        while (left < right) {
+            res = Math.max(res, Math.min(height[left], height[right]) * (right - left));
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return  res;
+    }
 }
