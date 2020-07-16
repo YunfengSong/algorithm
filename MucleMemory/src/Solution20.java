9203916af51c4b5cad1ed161d8e3dc0e27e22fe8
songyunfeng <56113272@qq.com>
2020-07-16 23:20:35 +0800

    July 16th.

--- MucleMemory/src/Solution20.java
+++ MucleMemory/src/Solution20.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 /**
  * @author: Yunfeng
  * @create: 2020-07-16
@@ -9,5 +11,24 @@
  * 输出: false
  **/
 public class Solution20 {
+    public boolean isValid(String s) {
+        if (s == null || (s.length() & 1) == 1) {
+            return false;
+        }
 
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '[' || s.charAt(i) == '(' || s.charAt(i) == '{') {
+                stack.push(s.charAt(i));
+            } else if (s.charAt(i) == ']' && (stack.isEmpty() || stack.pop() != '[')) {
+                return false;
+            } else if (s.charAt(i) == '}' && (stack.isEmpty() || stack.pop() != '{')) {
+                return false;
+            } else if (s.charAt(i) == ')' && (stack.isEmpty() || stack.pop() != '(')) {
+                return false;
+            }
+
+        }
+        return stack.isEmpty();
+    }
 }
