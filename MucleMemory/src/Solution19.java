2c552d8031ea02d95ba2ced0ac5eedfcdb06bdb7
songyunfeng <56113272@qq.com>
2020-07-16 22:39:47 +0800

    July 16th.

---
+++ MucleMemory/src/Solution19.java
@@ -0,0 +1,27 @@
+import java.util.List;
+
+/**
+ * @author: Yunfeng
+ * @create: 2020-07-16
+ * <p>
+ * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+ * <p>
+ * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ **/
+public class Solution19 {
+    public ListNode removeNthFrormEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode fast = head;
+        ListNode slow = dummy;
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
