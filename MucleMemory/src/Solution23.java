f01c83eafe5a5f1342db6435938a7aa96abd0c1c
yunfeng <yunfeng.syf@alibaba-inc.com>
2020-07-22 08:59:01 +0800

    July22yh.

--- MucleMemory/src/Solution23.java
+++ MucleMemory/src/Solution23.java
@@ -1,6 +1,35 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 /**
  * @author: Yunfeng
  * @create: 2020-07-21
  **/
 public class Solution23 {
+    public ListNode mergeLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode pre = dummy;
+
+        Queue<ListNode> minHep = new PriorityQueue<>(Comparator.comparing(node -> node.val));
+        for (ListNode list : lists) {
+            if (list != null) {
+                minHep.offer(list);
+            }
+        }
+
+        while (!minHep.isEmpty()) {
+            pre.next = minHep.poll();
+            pre = pre.next;
+            if (pre.next != null) {
+                minHep.offer(pre.next);
+            }
+        }
+
+        return dummy.next;
+    }
 }
--- MucleMemory/src/Solution31.java
+++ MucleMemory/src/Solution31.java
@@ -4,14 +4,43 @@
  * @Date 2020/7/18
  */
 public class Solution31 {
+    public void nextPermutation(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+        int firstSmall = -1;
+        for (int i = nums.length - 2; i >= 0; i--) {
 
-    public static void main(String[] args) {
-        System.out.println("A");
+            if (nums[i] < nums[i + 1]) {
+                firstSmall = i;
+                break;
+            }
+        }
+        if (firstSmall == -1) {
+            reverse(nums, 0, nums.length - 1);
+            return;
+        }
+
+        for (int i = nums.length - 1; i > firstSmall; i--) {
+            if (nums[i] > nums[firstSmall]){
+                swap(nums, i, firstSmall);
+                reverse(nums, firstSmall + 1, nums.length - 1);
+                return;
+            }
+        }
+
     }
 
-    public void B() {
-
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start++, end--);
+        }
+    }
 
 }
